8cfe0c6d3e2e0c1c37c69990a1004a483567911a
mrtkrkrt <muratkrkrt0711@gmail.com>
2023-06-25 12:24:29 +0300

    FormAuth Refactoring

--- src/main/java/com/springsecurity/demo/FormAuth/FormAuthController.java
+++ src/main/java/com/springsecurity/demo/FormAuth/FormAuthController.java
@@ -1,10 +1,10 @@
-package com.springsecurity.demo;
+package com.springsecurity.demo.FormAuth;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class HelloWorldController {
+public class FormAuthController {
 
     @GetMapping("hello-world")
     public String helloWorld() {
